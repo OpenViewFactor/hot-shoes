e18a2a952c6dbf3dd513a847152a8ce48f7d7a27
NatanHerzog <natanherzog@gmail.com>
2024-10-23 20:30:30 -0400

    adding meshes for the mli and new function to generate the mli meshes

---
+++ mesh-generation/generate_mli_units.m
@@ -0,0 +1,82 @@
+function generate_mli_units(REFINE_SCALE, EXPORT_TOGGLE, GRAPH_TOGGLE)
+  arguments
+    REFINE_SCALE int8 {mustBePositive, mustBeInteger} = 2
+    EXPORT_TOGGLE logical {mustBeNumericOrLogical} = false
+    GRAPH_TOGGLE logical {mustBeNumericOrLogical} = true
+  end
+
+  %* --------------- SHOE PARAMETERS --------------- *%
+  SHOE_SIZE = 0.9;              % [in]
+  VERT_SPACING = 0.145;         % [in]
+  CENTER_OFFSET = 79.003/25.4;  % [in]
+  SIDE_LENGTH = 65.446/25.4;    % [in]
+  SHOE_STACK_COUNT = 18;
+
+  %* --------------- GENERATE HOT SHOES --------------- *%
+  %! generate hot shoe face parallel to the x-z plane
+  planar_corner = [0, CENTER_OFFSET, 0];
+  x_z_positive_y_face_vertices = [planar_corner; ...
+                                  planar_corner(1) + SIDE_LENGTH/2, planar_corner(2), planar_corner(3); ...
+                                  planar_corner(1) + SIDE_LENGTH/2, planar_corner(2), planar_corner(3) + SHOE_SIZE + VERT_SPACING; ...
+                                  planar_corner(1), planar_corner(2), planar_corner(3) + SHOE_SIZE + VERT_SPACING];
+  x_z_positive_y_face_mesh = generate_rectangle(x_z_positive_y_face_vertices, REFINE_SCALE);
+  %! generate hot shoe face parallel to the y-z plane
+  y_z_positive_x_face_mesh = flipMeshAboutPlane(x_z_positive_y_face_mesh, [1,1,1], [-1,1,0]);
+  
+  %! generate hot shoe faces along the diagonal between the above two meshes
+  diagonal_corner = [planar_corner(1) + SIDE_LENGTH/2, planar_corner(2), planar_corner(3)];
+  left_diagonal_face_vertices = [diagonal_corner; ...
+                                 diagonal_corner(1) + (SIDE_LENGTH/sqrt(2))/2, diagonal_corner(2) - (SIDE_LENGTH/sqrt(2))/2, diagonal_corner(3); ...
+                                 diagonal_corner(1) + (SIDE_LENGTH/sqrt(2))/2, diagonal_corner(2) - (SIDE_LENGTH/sqrt(2))/2, diagonal_corner(3) + SHOE_SIZE + VERT_SPACING;...
+                                 diagonal_corner(1), diagonal_corner(2), diagonal_corner(3) + SHOE_SIZE + VERT_SPACING];
+  left_diagonal_face_mesh = generate_rectangle(left_diagonal_face_vertices, REFINE_SCALE);
+  right_diagonal_face_mesh = translateMesh(left_diagonal_face_mesh, (SIDE_LENGTH/2) * [1/sqrt(2), -1/sqrt(2), 0]);
+
+  %* --------------- DUPLICATE HOT SHOES VERTICALLY --------------- *%
+  MLI_meshes = cell(SHOE_STACK_COUNT,4);
+  MLI_meshes{SHOE_STACK_COUNT, 1} = x_z_positive_y_face_mesh;
+  MLI_meshes{SHOE_STACK_COUNT, 2} = left_diagonal_face_mesh;
+  MLI_meshes{SHOE_STACK_COUNT, 3} = right_diagonal_face_mesh;
+  MLI_meshes{SHOE_STACK_COUNT, 4} = y_z_positive_x_face_mesh;
+
+  for i = 1 : (SHOE_STACK_COUNT) - 1
+    MLI_meshes{SHOE_STACK_COUNT - i, 1} = translateMesh(x_z_positive_y_face_mesh, [0, 0, i*(SHOE_SIZE + VERT_SPACING)]);
+    MLI_meshes{SHOE_STACK_COUNT - i, 2} = translateMesh(left_diagonal_face_mesh, [0, 0, i*(SHOE_SIZE + VERT_SPACING)]);
+    MLI_meshes{SHOE_STACK_COUNT - i, 3} = translateMesh(right_diagonal_face_mesh, [0, 0, i*(SHOE_SIZE + VERT_SPACING)]);
+    MLI_meshes{SHOE_STACK_COUNT - i, 4} = translateMesh(y_z_positive_x_face_mesh, [0, 0, i*(SHOE_SIZE + VERT_SPACING)]);
+  end
+
+  %* --------------- EXPORT MESHES --------------- *%
+  if EXPORT_TOGGLE
+    if ~exist(['../assets/mli/ref-',num2str(REFINE_SCALE)], 'dir')
+        mkdir(['../assets/mli/ref-',num2str(REFINE_SCALE)])
+    end
+    for i = 1 : SHOE_STACK_COUNT
+        stlwrite(MLI_meshes{i, 1}, ['../assets/mli/ref-',num2str(REFINE_SCALE),'/x_z_positive_y_face-row-',num2str(i),'-ref-',num2str(REFINE_SCALE),'.stl'], "binary")
+        stlwrite(MLI_meshes{i, 2}, ['../assets/mli/ref-',num2str(REFINE_SCALE),'/left_diagonal_face-row-',num2str(i),'-ref-',num2str(REFINE_SCALE),'.stl'], "binary")
+        stlwrite(MLI_meshes{i, 3}, ['../assets/mli/ref-',num2str(REFINE_SCALE),'/right_diagonal_face-row-',num2str(i),'-ref-',num2str(REFINE_SCALE),'.stl'], "binary")
+        stlwrite(MLI_meshes{i, 4}, ['../assets/mli/ref-',num2str(REFINE_SCALE),'/y_z_positive_x_face-row-',num2str(i),'-ref-',num2str(REFINE_SCALE),'.stl'], "binary")
+    end
+  end
+
+  %* --------------- PLOT MESHES --------------- *%
+  if GRAPH_TOGGLE
+    
+    figure
+    hold on
+    for i = 1 : SHOE_STACK_COUNT
+      trisurf(MLI_meshes{i, 1},'EdgeColor',[0,0,1].*mod(i,2),'LineStyle','-','FaceColor',[1,0,0].*i/SHOE_STACK_COUNT)
+      trisurf(MLI_meshes{i, 2},'EdgeColor',[0,1,0].*mod(i,2),'LineStyle','-','FaceColor',[1,1,0].*i/SHOE_STACK_COUNT)
+      trisurf(MLI_meshes{i, 3},'EdgeColor',[1,1,0].*mod(i,2),'LineStyle','-','FaceColor',[0,1,0].*i/SHOE_STACK_COUNT)
+      trisurf(MLI_meshes{i, 4},'EdgeColor',[1,0,0].*mod(i,2),'LineStyle','-','FaceColor',[0,0,1].*i/SHOE_STACK_COUNT)
+    end
+    pbaspect([1,1,1])
+    daspect([1,1,1])
+    xlabel('x')
+    ylabel('y')
+    zlabel('z')
+    view(3)
+
+  end
+
+end
